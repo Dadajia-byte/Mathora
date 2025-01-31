88ff0a131164662eafb35a0957a57209a3059753
Mr-East <dearwxd812@gmail.com>
2025-01-31 23:15:03 +0800

    feat: 实现响应式

--- src/utils/flexible.ts
+++ src/utils/flexible.ts
@@ -1,16 +1,11 @@
 // 蓝湖上设计稿自定义为1920px 测量值直接写入即可
-
-const baseSize = 192; //设计稿宽度%10 比如 1920 
-
-// 设置rem 函数
+const baseSize = 16
 function setRem() {
-//计算出 比例来 当前分辨率的宽%设计稿宽度
-  const scale = window.screen.width / 1920;
-  // 给根元素设置font-size
-  document.documentElement.style.fontSize =
-    baseSize * Math.min(scale, 2) + "px";
+  const scale = document.documentElement.clientWidth / 1920
+  document.documentElement.style.fontSize = baseSize * scale + 'px'
 }
- setRem();
-  window.onresize = function () {
-    setRem();
-  };
+setRem()
+window.onresize = function () {
+  setRem()
+}
+
