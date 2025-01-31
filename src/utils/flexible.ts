65fb1ae04901d689c3a59f14a60f72365233b757
Mr-East <dearwxd812@gmail.com>
2025-01-31 23:28:17 +0800

    feat: 更新flexible hook

--- src/utils/flexible.ts
+++ src/utils/flexible.ts
@@ -1,11 +1,46 @@
 // 蓝湖上设计稿自定义为1920px 测量值直接写入即可
-const baseSize = 16
+const baseSize = 16;
 function setRem() {
-  const scale = document.documentElement.clientWidth / 1920
-  document.documentElement.style.fontSize = baseSize * scale + 'px'
-}
-setRem()
-window.onresize = function () {
-  setRem()
+  const scale = document.documentElement.clientWidth / 1920;
+  document.documentElement.style.fontSize = baseSize * scale + "px";
 }
 
+(function flexible(window, document) {
+  var docEl = document.documentElement;
+  var dpr = window.devicePixelRatio || 1; // adjust body font size
+
+  function setBodyFontSize() {
+    if (document.body) {
+      document.body.style.fontSize = 12 * dpr + "px";
+    } else {
+      document.addEventListener("DOMContentLoaded", setBodyFontSize);
+    }
+  }
+  setBodyFontSize(); // set 1rem = viewWidth / 10
+
+  function setRemUnit() {
+    var rem = docEl.clientWidth / 24; // 这里默认是10等份，手动改为24，此时1rem=1920/24px即80px。（设计稿是1920px的）
+    docEl.style.fontSize = rem + "px";
+  }
+
+  setRem(); // reset rem unit on page resize
+
+  window.addEventListener("resize", setRem);
+  window.addEventListener("pageshow", function (e) {
+    if (e.persisted) {
+      setRemUnit();
+    }
+  }); // detect 0.5px supports
+
+  if (dpr >= 2) {
+    var fakeBody = document.createElement("body");
+    var testElement = document.createElement("div");
+    testElement.style.border = ".5px solid transparent";
+    fakeBody.appendChild(testElement);
+    docEl.appendChild(fakeBody);
+    if (testElement.offsetHeight === 1) {
+      docEl.classList.add("hairlines");
+    }
+    docEl.removeChild(fakeBody);
+  }
+})(window, document);
