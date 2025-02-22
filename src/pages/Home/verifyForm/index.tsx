ba34704a5928b83572c88583681bc552a3e894c8
Mr-East <32808240+jsjzyzc@users.noreply.github.com>
2025-02-22 16:01:26 +0800

    feat: 登录页动画实现

---
+++ src/pages/Home/verifyForm/index.tsx
@@ -0,0 +1,65 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  useremail: z.string().min(2, {
+    message: "请输入正确的邮箱",
+  }),
+});
+
+export function VerifyForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      useremail: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full h-full ">
+        <FormField
+          control={form.control}
+          name="useremail"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder="请输入验证码"
+                  {...field}
+                  className="w-[500px] h-[67px] box-border border-[#1F2A3D] border-[1px] rounded-[12px] text-[#94A3B8] text-[24px] placeholder:text-[#94A3B8] placeholder:text-[24px]"
+                />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="w-[500px] box-border h-[67px] mt-[25px] flex justify-center content-center items-center bg-[#3C83F6] rounded-[12px] text-[#FFFFFF] text-[24px] hover:bg-sky-700"
+        >
+          使用邮箱继续
+        </Button>
+      </form>
+    </Form>
+  );
+}
+//
